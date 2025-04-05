4bda682cf43cd20d9902e7e186928e76d649b5d4
satyam1125 <satyam.17768@sakec.ac.in>
2025-04-05 16:11:24 +0530

    huh

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,24 @@
 /** @type {import('next').NextConfig} */
+import type { Configuration } from 'webpack';
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  },
   images: {
     domains: [
-      'i.pinimg.com', 
-      'images.unsplash.com', 
+      'i.pinimg.com',
+      'images.unsplash.com',
       'dynamic-media-cdn.tripadvisor.com',
       'images.bhaskarassets.com',
-      'photos.hotelbeds.com'
+      'photos.hotelbeds.com',
+      'cloud.appwrite.io'
     ],
   },
   async headers() {
     return [
-      {
+      {        
         source: '/(.*)',
         headers: [
           {
@@ -47,7 +53,17 @@ const nextConfig = {
       },
     ];
   },
+  webpack: (config: Configuration): Configuration => {
+    config.resolve = config.resolve ?? {};
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react$': require.resolve('react'),
+      'react-dom$': require.resolve('react-dom')
+    };
+    return config;
+  }
 };
+
 
 export default nextConfig;
 
